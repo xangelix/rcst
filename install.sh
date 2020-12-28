e445618890d333d103081745f8f593ea5898081d
Cody Neiman <neiman1010222@gmail.com>
2020-12-27 22:48:38 -0500

    Initial installation implementation

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Finds the path of this install.sh
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sudo chmod +x $BASE_DIR/rcst.py
+
+cp $BASE_DIR/rclone_share_tools.desktop $BASE_DIR/temp.desktop
+
+sed -i "s+/this/path/should/be/replaced/on/install+$BASE_DIR/rcst.py+g" $BASE_DIR/temp.desktop
+sed -i "s+dst_route_1+$1+g" $BASE_DIR/temp.desktop
+sed -i "s+dst_dir_1+$2+g" $BASE_DIR/temp.desktop
+
+sudo rm -f /usr/share/kservices5/ServiceMenus/rclone_share_tools.desktop
+
+sudo cp $BASE_DIR/temp.desktop /usr/share/kservices5/ServiceMenus/rclone_share_tools.desktop
+
+rm $BASE_DIR/temp.desktop
+
+echo "Done"
