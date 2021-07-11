b8ac95840784245915ad6e304275eaa4a3b2ec05
Cody Neiman <cody-neiman@cody.fun>
2021-07-11 23:04:36 +0000

    Add additional Gitpod signing info to script

--- .gitpod.init_signing.sh
+++ .gitpod.init_signing.sh
@@ -3,7 +3,10 @@
 mkdir -p /home/gitpod/.ssh/
 echo $SSH_KEY_B64 | base64 -di > /home/gitpod/.ssh/id_rsa
 echo "--SSH Private Key Imported--"
+echo "=============================================================="
+echo "                YOU MUST ENTER PASSWORD TWICE                 "
 echo "IF YOU ENTER PASSSWORD WRONG, RUN: './.gitpod.init_signing.sh'"
+echo "=============================================================="
 echo $GPG_KEY_B64 | base64 -di > /home/gitpod/pkey.key
 gpg --pinentry-mode=loopback --import /home/gitpod/pkey.key
 echo "--GPG Private Key Imported--"
